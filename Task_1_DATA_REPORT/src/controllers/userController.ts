71212ae464ce4c4fadf636a9416e4db9ed9e8b08
LeoPKM21 <thinhphatmai2001@gmail.com>
2024-10-10 04:47:53 +0700

    thieu xoa file sau khi cap nhat

--- Task_1_DATA_REPORT/src/controllers/userController.ts
+++ Task_1_DATA_REPORT/src/controllers/userController.ts
@@ -6,17 +6,23 @@ export const userUpFileController = async (
     next: NextFunction
 ) => {
     const username: string = req.body.username;
-    const filename: string = req.file!.filename;
+    const newFileName: string = req.file!.filename;
     const userModel = new UserModel();
     const isExisted = await userModel.isUserExisted(username);
-    console.log("Done_" + req.file?.filename);
+    console.log("\n\ninsert file" + newFileName);
     console.log("username", username);
     if (isExisted) {
         // user has upload file before
         console.log("user _ exist");
+        let oldFilename = await userModel.getFilenameByUsername(username);
+        if (typeof oldFilename == "string") {
+            userModel.updateFileNameOfUser(username, newFileName);
+            console.log({oldFilename});
+            
+        }
     } else {
         // new user
         console.log("create entry");
-        userModel.insertDataIntoUserFileTable(username, filename);
+        userModel.insertDataIntoUserFileTable(username, newFileName);
     }
 };
--- Task_1_DATA_REPORT/src/models/userModel.ts
+++ Task_1_DATA_REPORT/src/models/userModel.ts
@@ -1,5 +1,10 @@
 import { DatabaseHandler } from "./database";
 import { USER_FILE_TABLE_NAME } from "./../utils/config";
+
+interface User_File_Row_InterFace {
+    username: string;
+    filename: string;
+}
 export class UserModel {
     isUserExisted(username: string): Promise<boolean> {
         const dbManipulate = new DatabaseHandler();
@@ -29,5 +34,43 @@ export class UserModel {
             `INSERT INTO ${USER_FILE_TABLE_NAME} (username, filename) VALUES (?, ?)`,
             [username, fileName]
         );
+    }
+
+    async updateFileNameOfUser(username: string, updateFileName: string) {
+        const dbManipulate = new DatabaseHandler();
+        await dbManipulate.runQuery(
+            `UPDATE ${USER_FILE_TABLE_NAME} SET filename = ? WHERE username = ?`,
+            [updateFileName, username]
+        );
+    }
+
+    async getFilenameByUsername(username: string): Promise<string | undefined> {
+        const dbManipulate = new DatabaseHandler();
+        const db = dbManipulate.getConnector();
+        try {
+            return await new Promise<string | undefined>((resolve, reject) => {
+                db.get(
+                    `SELECT filename FROM ${USER_FILE_TABLE_NAME} WHERE username = ?`,
+                    [username],
+                    (err, row: User_File_Row_InterFace) => {
+                        dbManipulate.closeDB(db); // Close the connection after the query
+
+                        if (err) {
+                            console.error(
+                                "Error retrieving filename:",
+                                err.message
+                            );
+                            reject(err);
+                        } else if (!row) {
+                            resolve(undefined); // If no record found, resolve to null
+                        } else {
+                            resolve(row.filename ?? undefined); // Return the single filename
+                        }
+                    }
+                );
+            });
+        } catch (error) {
+            console.error("Error in async function:", error);
+        }
     }
 }
